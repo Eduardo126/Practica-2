95f1d5f0b723a8b536d861003db3a44100a4bc6b
practica 2 <Computosala42022@ugb.edu.sv>
2022-08-12 11:50:39 -0600

    app4

--- App1/Program.cs
+++ App1/Program.cs
@@ -6,7 +6,31 @@ namespace App1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ingrese su nota promedio");
+            double nota = double.Parse(Console.ReadLine());
+
+            if (nota >= 0 && nota <= 10)
+            {
+                if (nota >= 6)
+                {
+                    Console.WriteLine("Aprobado");
+                } 
+                else if (nota >= 5)
+                {
+                    Console.WriteLine("Reposicion");
+
+                }
+                else
+                {
+                    Console.WriteLine("Reprobado");
+
+                }
+            
+            }
+            else
+            {
+                Console.WriteLine("La nota es correcta");
+            }
         }
     }
 }
